552f4ccf0bcde6a55bb8024553e2e8b755f97c5c
Malu <mlmv@cin.ufpe.br>
2018-12-04 23:11:31 -0300

    fazendo uma tela apontar pra outra

--- app/app/src/main/java/br/ufpe/cin/levapramim/PathActivity.kt
+++ app/app/src/main/java/br/ufpe/cin/levapramim/PathActivity.kt
@@ -1,12 +1,11 @@
 package br.ufpe.cin.levapramim
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,23 +13,22 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class PathActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        setContentView(R.layout.activity_path)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        var bt = findViewById(R.id.receber) as Button
-        bt.setOnClickListener({ receiveDialog() })
+        var bt = findViewById(R.id.finalizar) as Button
+        bt.setOnClickListener({ finalDialog() })
     }
-    
+
 
 
     /**
@@ -51,28 +49,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
-    fun receiveDialog() {
-        //esse dialog vai abrir qdo receber a solicitação do usuário. a requisição vai conter o de/para para ser printado
-        val de = "lugar A"
-        val para = "lugar B"
+    fun finalDialog() {
+        //esse dialog vai ser aberto quando clicar no botão de finalizar corrida.
+        val valor = "15,00"
         val alertDialog = AlertDialog.Builder(this)
-        alertDialog.setTitle("Nova corrida") // O Titulo da notificação
-        alertDialog.setMessage("deseja aceitar a corrida de $de para $para?") // a mensagem ou alerta
-
-        //quando o botão de aceitar for clicado, vai redirecionar pra tela dizendo a localização a qual ele tem que se dirigir.
+        alertDialog.setTitle("Final da corrida") // O Titulo da notificação
+        alertDialog.setMessage("O valor da corrida foi $valor?") // a mensagem ou alerta
+        //depois do dinheiro recebido, volta pra tela inicial
         alertDialog.setPositiveButton("Sim") { _, _ ->
-                startActivity( Intent(this, PathActivity::class.java) )
+            startActivity( Intent(this, MapsActivity::class.java) )
 
         }
-        alertDialog.setNegativeButton("Não") { _, _ -> Toast.makeText(this, "Não", Toast.LENGTH_LONG).show() }
+        alertDialog.setNegativeButton("Não recebido") { _, _ -> Toast.makeText(this, "Deu ruim", Toast.LENGTH_LONG).show() }
 
         alertDialog.show()
-
-//        val intent = Intent(this, DisplayMessageActivity::class.java)
-//        val editText = findViewById(R.id.editText) as EditText
-//        val message = editText.text.toString()
-//        intent.putExtra(EXTRA_MESSAGE, message)
-//        startActivity(intent)
     }
 
 }
