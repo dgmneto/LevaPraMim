9a09bfdeb39ddc2d6f10bff9ace1d45d1b95d83d
Malu <mlmv@cin.ufpe.br>
2018-12-06 21:44:11 -0300

    funcionando quase tudo exceto o spinner

--- app/app/src/main/java/br/ufpe/cin/levapramim/WaitingPorter.kt
+++ app/app/src/main/java/br/ufpe/cin/levapramim/WaitingPorter.kt
@@ -1,30 +1,38 @@
 package br.ufpe.cin.levapramim
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_waiting_porter.*
 
 
 class WaitingPorter : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    var chegou = "o carinha chegou"
+    var procurando = "procurando o carinha"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_call)
+        setContentView(R.layout.activity_waiting_porter)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         //renderiza o texto de que tá aguardando algum carregador disponível
-        //botar um timer antes de chamar o próximo texto
-        //quando encontra o carregador, bota as infos dele na tela e dizendo que ele já chegou
+        val textViewToChange = findViewById(R.id.status) as TextView
+        textViewToChange.text = procurando
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -37,4 +45,13 @@ class WaitingPorter : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putString(procurando, status.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    fun changeStatus() {
+        val textViewToChange = findViewById(R.id.status) as TextView
+        textViewToChange.text = chegou
+    }
 }
